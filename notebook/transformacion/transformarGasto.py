9a50f0698f2efc7f582afb0e358e353e05b98298
ThomasCT15 <thomas.ci152003@gmail.com>
2026-06-02 18:14:00 -0500

    transformacion de usuario realizada

--- notebook/transformacion/transformarGasto.py
+++ notebook/transformacion/transformarGasto.py
@@ -1,8 +1,37 @@
-#def transformar_gastos():
+def transformar_gastos(df_gastos_limpio):
 
-#Filtrar los gastos con monto mayor a 50000 agrupados por descripcion (Barras)
+    #Filtrar los gastos con monto mayor a 50000 agrupados por descripcion (Barras)
+    filtro1=df_gastos_limpio.query("monto > 50000")
+    agrupacion1=filtro1.groupby("descripcion")["id"].count().reset_index()
+    agrupacion1.columns=["descripcion","cantidad"]
 
-#Todos los gastos agrupados por fecha (Lineas) (sumar)
+    #Todos los gastos agrupados por fecha (Lineas) (sumar)
+    filtro2=df_gastos_limpio.query("fecha == fecha")
+    agrupacion2=filtro2.groupby("fecha")["monto"].sum().reset_index()
+    agrupacion2.columns=["fecha","total"]
+
+    #Todos los gastos agrupados por descripcion (Torta) (sumar)
+    filtro3=df_gastos_limpio.query("descripcion == descripcion")
+    agrupacion3=filtro3.groupby("descripcion")["monto"].sum().reset_index()
+    agrupacion3.columns=["descripcion","total"]
+
+    #Gastos menores a 100000 agrupados por descripcion (Barras) (promedio)
+    filtro4=df_gastos_limpio.query("monto < 100000")
+    agrupacion4=filtro4.groupby("descripcion")["monto"].mean().reset_index()
+    agrupacion4.columns=["descripcion","promedio"]
+
+    #Gastos con monto entre 1000 y 20000 agrupados por fecha (Lineas) (contar)
+    filtro5=df_gastos_limpio.query("monto >= 1000 and monto <= 20000")
+    agrupacion5=filtro5.groupby("fecha")["id"].count().reset_index()
+    agrupacion5.columns=["fecha","cantidad"]
+
+    agrupaciones={
+        "agrupacion1":agrupacion1,
+        "agrupacion2":agrupacion2,
+        "agrupacion3":agrupacion3,
+        "agrupacion4":agrupacion4,
+        "agrupacion5":agrupacion5
+    }
 
 #Todos los gastos agrupados por descripcion (Torta) (sumar)
 
